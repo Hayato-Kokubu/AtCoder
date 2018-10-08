4feede5ae5fc97ae0e5ba1bcbbd3c9fb8d7de886
Hayato-Kokubu <kokubu.hayato@gmail.com>
2018-10-08 10:49:12 +0900

    約数の個数を求めるメソッド diviserNumを実装した コメントを下にまとめた

--- src/main/scala/ABC/_105/A/Main.scala
+++ src/main/scala/ABC/_105/A/Main.scala
@@ -1,65 +1,74 @@
 package ABC._105.A
+//↑実行時には外す
 
 object Main{
 	def main(args: Array[String]): Unit = {
 
-		val seq = Seq(1, 2, 3, 4, 5)
-		val range = 1 to 9
-
-		//　foreach
-		// Seq[Int] => Unit
-		/*
-		seq.foreach{ num =>
-			println(num)
-		}
-*/
-
-		// fold (= foldLeft)
-		// Seq[Int] => Int (和を求める)
-
-		//		val sum = seq.foldLeft(0){ (result, num)  =>
-		//			result + num
-		//		}
-
-		// 1 - 105 で、割り切れた回数を知る
-		// 2 - sqrt(105)までで、割れた回数を記録
-		//
-
 		val N = 105
+
+		val result: Int = diviserNum(N)
+		println(result)
+	}
+
+	def log(base: Int)(trueVal: Int): Int = {
+		var i = 0
+		var trueVar = trueVal
+
+		while( trueVar % base == 0 ) {
+			i = i + 1
+			trueVar = trueVar / base
+		}
+		i
+	}
+
+	def diviserNum(N: Int): Int = {
+		val inclusive = 2 to (Math.sqrt(N).toInt + 1)
 		var Nvar = N
 
-		val inclusive = 2 to (Math.sqrt(N).toInt + 1)
+		val resultList = inclusive.map { num =>
+			val result_i = log(num)(Nvar)
+			Nvar = Nvar / Math.pow(num.toDouble, result_i.toDouble).toInt
+			result_i
+		}
+		val result =
+			resultList.filter(_ > 0).foldLeft(1) { (rs, num) =>
+				rs * (num + 1)
+			}
+		result
+	}
+}
 
+// memo
+
+/*
+val seq = Seq(1, 2, 3, 4, 5)
+val range = 1 to 9
+
+//　foreach
+// Seq[Int] => Unit
+/*
+seq.foreach{ num =>
+	println(num)
+}
+*/
+
+// fold (= foldLeft)
+// Seq[Int] => Int (和を求める)
+
+//		val sum = seq.foldLeft(0){ (result, num)  =>
+//			result + num
+//		}
+
+// 1 - 105 で、割り切れた回数を知る
+// 2 - sqrt(105)までで、割れた回数を記録
+//
+*/
 
 //		val (result_i, NVal) = log(N)(Nvar)
 //		(result_i, NVal)
 //		ではエラー Nvalがみつからないと怒られる
 //類似例: http://xawa99.blogspot.com/2012/09/Scala-MatchBasic.html
-
-		val resultList = inclusive.map{ num =>
-			val result_i = log(num)(Nvar)
-			Nvar = Nvar / Math.pow(num.toDouble, result_i.toDouble).toInt
-			result_i
-		}
-
-		val resultListFilterd = resultList.filter( _ > 0)
-
-		val result = resultListFilterd.foldLeft(1){ (rs, num) =>
-//			println((rs, num))
-			rs * (num + 1)
-		}
-
 //Scala に置けるthis って何者？　self　もいたな
-
-//		println(inclusive.toVector)
-//		println(resultList)
-//		println(resultListFilterd)
-		println(result)
-
-
-
-	}
-
 //    IntelliJ からのオススメ　いい感じにかく
 //		List(1, 2, 3).foldLeft(1){(x,y) => x * y}
 //		List(1, 2, 3).fold(0)(_ + _)
@@ -73,16 +82,4 @@ object Main{
 //		List(1, 2, 3).min
 //		List(1, 2, 3).max
 
-	//foldと仲良くなる
-
-	def log(base: Int)(trueVal: Int): Int = {
-		var i = 0
-		var trueVar = trueVal
-
-		while( trueVar % base == 0 ) {
-			i = i + 1
-			trueVar = trueVar / base
-		}
-		i
-	}
-}
+//foldと仲良くなる
