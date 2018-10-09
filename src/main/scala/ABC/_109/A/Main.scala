90b113338c998d9aa17ae50073f4a69c3aff0e1e
Hayato-Kokubu <kokubu.hayato@gmail.com>
2018-10-10 05:57:43 +0900

    109.A 実装完了 -> AC

---
+++ src/main/scala/ABC/_109/A/Main.scala
@@ -0,0 +1,14 @@
+package ABC._109.A
+
+import scala.io.StdIn
+
+object Main {
+	def main(args: Array[String]): Unit ={
+		val num = StdIn.readLine.split(" ").map(_.toInt)
+		val A = num(0)
+		val B = num(1)
+
+		val result = if(A * B % 2 == 1) "Yes" else "No"
+		println(result)
+	}
+}
