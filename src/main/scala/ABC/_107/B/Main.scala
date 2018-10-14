8be9447e01239527f8e14cce35ef88f8b4c3e9a5
Hayato-Kokubu <kokubu.hayato@gmail.com>
2018-10-14 22:37:53 +0900

    ABC 107 B AC リファクタありそう

--- src/main/scala/ABC/_107/B/Main.scala
+++ src/main/scala/ABC/_107/B/Main.scala
@@ -1,16 +1,31 @@
 //package ABC._107.B
 
-import scala.io.{Source, StdIn}
+import scala.io.StdIn
 
 object Main {
 	def main(args: Array[String]): Unit = {
-		val num =  StdIn.readLine.split(" ").map(_.toInt)
+		val num = StdIn.readLine.split(" ").map(_.toInt)
 		val H = num(0)
 		val W = num(1)
 
-//		val grid = Source.stdin.getLines()
-		val grid = Source.stdin.mkString
-		println(grid)
-	}
+		val whiteLine = "....................................................................................................".substring(0, W)
 
+		val gridRows = (1 to H).foldLeft(Seq.empty[String]) { (seq, n) =>
+			seq :+ StdIn.readLine
+		}
+
+		val grsReducedRows = gridRows.filter(_ != whiteLine)
+		// 数字を使ってないが、うまくかけるのか？
+
+		val whiteColomns = grsReducedRows.foldLeft(whiteLine.toCharArray) { (res, str) =>
+			res.zip(str.toCharArray).map { tuple =>
+				if (tuple._1 == '.' && tuple._2 == '.') '.' else '#'
+			}
+		}
+
+		grsReducedRows.foreach { str =>
+			val resultRow = str.zip(whiteColomns).filter(_._2 == '#').map(_._1).foldLeft("")(_ + _)
+			println(resultRow)
+		}
+	}
 }
