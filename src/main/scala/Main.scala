7a88ea4b36be4fd14b26eacc2a8b07a640a6702e
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-03-29 06:32:27 +0900

    wip ABC028_B

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,18 @@
+import java.util.Scanner
+
 object Main {
+  val sc = new Scanner(System.in)
   def main (args: Array[String]): Unit={
-    println(Samples.gcd(12, 18))
+    val s  = sc.next
+    val ss = s.split("(?<=.)")
+
+    val na = s.count(_ == "A")
+    val nb = s.count(_ == "B")
+    val nc = s.count(_ == "C")
+    val nd = s.count(_ == "D")
+    val ne = s.count(_ == "E")
+    val nf = s.count(_ == "F")
+
+    println(s"$na, $nb, $nc, $nd, $ne, $nf")
   }
 }
