609566407838ca95eed37ae19668ee0070b717a8
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-03-28 22:25:34 +0900

    競技プログラミング用のライブラリとして、Sample.scalaを追加した。

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,5 @@
 object Main {
   def main (args: Array[String]): Unit={
-    println("Hello AtCoder!!")
+    println(Samples.gcd(12, 18))
   }
 }
---
+++ src/main/scala/Samples.scala
@@ -0,0 +1,46 @@
+object Samples {
+  /*
+  * @param  n   : Int
+  * @param  l   : l進数 (2 <= l <= 9)
+  * @return     : List[Int]
+  *               n のl進数表示
+  *               n = 8  の場合は を使う
+  *               n = 16 の場合は を使う
+  * 例: l_adic(15,2) = List(1,1,1,1)
+  *     l_adic(18,4) = List(1,0,2)
+  * */
+  def l_adic(n: Int, l: Int): List[Int] ={
+    def loop(m: Int, acc: List[Int]): List[Int] ={
+      m match {
+        case 0 => acc
+        case x => loop( x / l, (x % l) +: acc)
+      }
+    }
+    loop(n,List.empty[Int])
+  }
+
+  /*
+  * @param  a : Int a >= 1
+  * @param  b : Int b >= 1
+  *             a と b の大小関係は任意
+  * @return   : Int
+  *             a と b との最大公約数
+  * gcm(12,18) = 6
+  * gcm(4,9) = 1
+  * */
+  def gcd(a: Int, b: Int): Int ={
+    (a,b) match{
+      case (x,y) if y == 0 => x
+      case (x,y) if x < y  => gcd(y, x)
+      case (x,y)           => gcd(y, x % y)
+    }
+  }
+
+  def comulativeSum(intVector: Vector[Int]): Vector[Int] ={
+    intVector.foldLeft(Vector.empty[Int]){
+      case (list, acc) =>
+        if(list.isEmpty) Vector(acc)
+        else list :+ (list.last + acc)
+    }
+  }
+}
